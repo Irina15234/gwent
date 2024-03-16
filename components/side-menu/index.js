69fa1803d332d1a5416aae3765713bae405d240a
Irina15234 <irina15234@yandex.ru>
2024-03-16 20:28:58 +0300

    init

---
+++ components/side-menu/index.js
@@ -0,0 +1,19 @@
+function renderSideMenu() {
+    const sideMenu = document.getElementById("side-menu");
+
+    sideMenu.innerHTML = infoBlock({
+        title: 'Противник',
+        subtitle: 'Нильфгаард',
+        avatar: null,
+        cardsCount: 20,
+        livesCount: 2
+    });
+
+    sideMenu.innerHTML += infoBlock({
+        title: 'Геральт',
+        subtitle: 'Королевства севера',
+        avatar: null,
+        cardsCount: 20,
+        livesCount: 2
+    });
+}
---
+++ components/side-menu/info-block.js
@@ -0,0 +1,26 @@
+function infoBlock({title, subtitle, cardsCount, livesCount, avatar}) {
+  return `
+    <div class="info-block">
+      <div class="info-block__container">
+        <div class="info-block__avatar-container">
+          <div class="info-block__avatar" ></div>
+        </div>
+        <div>
+          <div>
+            <p class="info-block__title">${title}</p>
+            <p class="info-block__subtitle">${subtitle}</p>
+          </div>
+          <div>
+            <div class="info-block__cards">
+              <img width="24" height="24" src="../../assets/cards-filled.svg" alt="">
+              <span>${cardsCount}</span>
+            </div>
+            <div class="info-block__lives">
+              <img width="24" height="24" src="../../assets/red-gem.svg" alt="">
+              <img width="24" height="24" src="../../assets/red-gem.svg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`
+}
