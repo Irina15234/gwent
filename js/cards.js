c30f6eee3f26c41f68b87b52bf02e2b150782730
Irina15234 <irina15234@yandex.ru>
2024-08-01 00:11:04 +0300

    add cards

--- js/cards.js
+++ js/cards.js
@@ -1,30 +1,115 @@
 const playerCards = [
   {
-    row: 'warrior',
+    rows: ['warrior'],
     power: 10,
     additional: {},
     isGold: true,
     img: '../assets/cards/roshe.png'
   },
   {
-    row: 'archer',
+    rows: ['archer'],
     power: 10,
     additional: {},
     isGold: true,
-    img: '../assets/cards/roshe.png'
+    img: '../assets/cards/filippa.png'
+  },
+  {
+    rows: ['warrior'],
+    power: 10,
+    additional: {},
+    isGold: true,
+    img: '../assets/cards/natalis.png'
+  },
+  {
+    rows: ['warrior'],
+    power: 10,
+    additional: {},
+    isGold: true,
+    img: '../assets/cards/tissen.png'
+  },
+
+  {
+    rows: ['warrior'],
+    power: 5,
+    additional: {},
+    isGold: false,
+    img: '../assets/cards/stennis.png'
+  },
+  {
+    rows: ['artillery'],
+    power: 1,
+    additional: {},
+    isGold: false,
+    img: '../assets/cards/taler.png'
+  },
+  {
+    rows: ['artillery'],
+    power: 6,
+    additional: {},
+    isGold: false,
+    img: '../assets/cards/tower.png'
+  },
+  {
+    rows: ['artillery'],
+    power: 6,
+    additional: {},
+    isGold: false,
+    img: '../assets/cards/trebuchet.png'
+  },
+  {
+    rows: ['artillery'],
+    power: 6,
+    additional: {},
+    isGold: false,
+    img: '../assets/cards/trebuchet.png'
+  },
+  {
+    rows: ['warrior'],
+    power: 4,
+    additional: {},
+    isGold: false,
+    img: '../assets/cards/warriorBlueLine.png'
+  },
+  {
+    rows: ['warrior'],
+    power: 4,
+    additional: {},
+    isGold: false,
+    img: '../assets/cards/warriorBlueLine.png'
+  },
+  {
+    rows: ['archer'],
+    power: 5,
+    additional: {},
+    isGold: false,
+    img: '../assets/cards/warriorFromKrinfrid.png'
+  },
+  {
+    rows: ['archer'],
+    power: 5,
+    additional: {},
+    isGold: false,
+    img: '../assets/cards/warriorFromKrinfrid.png'
+  },
+  {
+    rows: ['warrior'],
+    power: 5,
+    additional: {},
+    isGold: false,
+    img: '../assets/cards/zigfrid.png'
   },
 ];
 
 const compCards = [
   {
-    row: 'archer',
+    rows: ['archer'],
     power: 2,
     additional: {},
     isGold: false,
     img: '../assets/cards/'
   },
   {
-    row: 'artillery',
+    rows: ['artillery'],
     power: 3,
     additional: {},
     isGold: false,
