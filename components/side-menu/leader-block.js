c48e5fdadca11b8183bd9093a7734182af40d1a8
Irina15234 <irina15234@yandex.ru>
2024-08-06 21:22:18 +0300

    add adaptive

--- components/side-menu/leader-block.js
+++ components/side-menu/leader-block.js
@@ -1,14 +1,8 @@
-function leaderBlock({leader, isActiveAction}) {
-  const actionClasses = 'leader-block__action' + (isActiveAction ? ' leader-block__action_active' : '');
-
+function leaderBlock({leader}) {
   return `
     <div class="leader-block">
       <div class="leader-block__container">
         <img class="leader-block__img" alt="" src=${leader.card} />
-      </div>
-      
-      <div class="${actionClasses}">
-        <div class="leader-block__action-button"><div class="leader-block__action-button-img"></div></div>
       </div>
     </div>
   `;
